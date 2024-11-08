9228d86304fe1d76e8cf4ca10170d2853fbd2d7a
brandon <2858207+igloo12@users.noreply.github.com>
2024-11-08 11:16:31 -0800

    commits

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ java {
 repositories {
     mavenCentral()
 }
-
+val ktorVersion = "2.3.12"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -27,6 +27,17 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.kafka:spring-kafka-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.ws:spring-ws-core")
+
+    // ----- KTOR -----
+    implementation("io.ktor:ktor-client-auth-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-xml:$ktorVersion")
+    implementation("io.ktor:ktor-client-encoding-jvm:$ktorVersion")
 }
 
 kotlin {
--- src/main/kotlin/com/chasecorp/kafkasuspend/KafkaSuspendApplication.kt
+++ src/main/kotlin/com/chasecorp/kafkasuspend/KafkaSuspendApplication.kt
@@ -1,11 +1,63 @@
 package com.chasecorp.kafkasuspend
 
+import io.ktor.client.plugins.*
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.listener.DefaultErrorHandler
+import org.springframework.stereotype.Component
+import org.springframework.util.backoff.FixedBackOff
 
 @SpringBootApplication
 class KafkaSuspendApplication
 
 fun main(args: Array<String>) {
     runApplication<KafkaSuspendApplication>(*args)
+}
+
+
+@Component
+class KafkaListeners(template: KafkaTemplate<String, String>) {
+    init {
+        template.send("test", "test")
+    }
+
+    // DOESN'T WORK
+    @KafkaListener(
+        id = "nocall",
+        groupId = "groupid",
+        topics = ["test"],
+    )
+    suspend fun doesntCall(message: String) {
+        throw HttpRequestTimeoutException("google.com", 100)
+    }
+
+    // WORKS
+    @KafkaListener(
+        id = "calls",
+        groupId = "groupid",
+        topics = ["test"],
+    )
+    fun calls(message: String) {
+        throw HttpRequestTimeoutException("google.com", 100)
+    }
+}
+
+@Configuration
+class ErrorConfig {
+    val logger = LoggerFactory.getLogger(ErrorConfig::class.java)
+
+    @Bean
+    fun defaultErrorHandler(): DefaultErrorHandler {
+        val defaultErrorHandler = DefaultErrorHandler()
+        defaultErrorHandler.setBackOffFunction { _, exception ->
+            logger.info("call me maybe")
+            FixedBackOff(2000, 100)
+        }
+        return defaultErrorHandler
+    }
 }
